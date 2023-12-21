6398d101d9c405c28afcbe15676c75d0c53376be
xiaofan <xiaofan@upsider-okada-mbp.local>
2023-12-22 01:16:03 +0900

    いろいろ

--- tools/latest-image-digest.sh
+++ tools/latest-image-digest.sh
@@ -9,7 +9,8 @@ base=$1
 
 # gcloud container images describe "$base":latest --format json 2>/dev/null |
 #     jq -M ".image_summary.fully_qualified_digest" |
-#     sed -e 's/\"//g'
+#     sed -e 's/\"//g' |
+#     cut -d '@' -f 2
 
 dummy_sha=$(openssl rand -hex 32 | openssl dgst -sha256 | cut -d " " -f 2)
-echo "$base@sha256:$dummy_sha"
+echo "sha256:$dummy_sha"
---
+++ tools/update-to-latest-image.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# If the value of a specified property in a YAML file is a container image name,
+# this script will update its digest to the latest version using yq.
+# After this replacement, the original file is overwritten.
+#
+# NOTE:
+# yq messes up the YAML's whitespace.
+# Please proceed with using this script, understanding this issue.
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <file-name> <property-path>"
+    exit 1
+fi
+
+file_name=$1
+property_path=$2
+
+current_image=$(yq eval "${property_path}" "$file_name")
+echo "Current image: $current_image"
+image_name=$(echo $current_image | cut -d '@' -f 1)
+
+# Digests contain the prefix 'sha256:'
+current_digest=$(echo $current_image | cut -d '@' -f 2)
+latest_digest=$("./tools/latest-image-digest.sh" "$image_name")
+echo "Latest digest: $latest_digest"
+
+if [ -z "$latest_digest" ]; then
+    echo "Error: Failed to get the latest image digest."
+    exit 1
+fi
+
+if [[ "$current_digest" == "$latest_digest" ]]; then
+    echo "The current image is the latest image, skipping."
+    exit 0
+fi
+
+yq eval --inplace "${property_path} = \"${image_name}@${latest_digest}\"" "$file_name"
