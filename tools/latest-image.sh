90ed22bad9f5043d411e91c7afbe7f1f0f93773a
xiaofan <xiaofan@upsider-okada-mbp.local>
2023-12-22 00:33:04 +0900

    sample1 workflow shows the latest image

---
+++ tools/latest-image.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <image-name>"
+    exit 1
+fi
+
+base=$1
+
+# gcloud container images describe "$base":latest --format json 2>/dev/null |
+#     jq -M ".image_summary.fully_qualified_digest" |
+#     sed -e 's/\"//g'
+
+dummy_sha=$(openssl rand -hex 32 | openssl dgst -sha256 | cut -d " " -f 2)
+echo "$base@sha256:$dummy_sha"
